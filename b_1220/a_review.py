c8ea807480c04d958df461f2dbdfaf6f4507c8fe
devgiants75 <devgiants75@naver.com>
2023-12-20 21:00:11 +0900

    20231220

---
+++ b_1220/a_review.py
@@ -0,0 +1,30 @@
+# 리뷰
+
+# 변수(Variable)
+# : 데이터를 저장하는 메모리의 이름
+
+# 사용법: 변수명 = 값
+
+# 숫자형 (Number)
+# 종류: int(정수), float(소수)
+x = 10
+y = 10.123
+
+# type()
+# : 데이터의 타입(자료형)을 반환하는 내장 함수
+print(type(x)) # <class 'int'>
+print(type(y)) # <class 'float'>
+
+# 문자열
+# : 텍스트 데이터를 나타내는 타입
+
+# 생성 방법
+# ''(작은 따옴표) ""(큰 따옴표) 
+name = 'Lee Seung Ah'
+print(type(name)) # <class 'str'>
+
+# 논리형
+# : 참(True) 또는 거짓(False)의 두 가지 값만을 가지는 데이터 타입
+# : 반드시 대문자로 시작
+is_student = True
+is_sunny = False
